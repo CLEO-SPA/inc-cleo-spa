14c41bde49324b2b791ad75420270fc50ae7215e
mintzic <minthitkhine2005@gmail.com>
2025-05-08 13:13:04 +0800

    refactor: streamline authentication and session routes by removing unused code and updating endpoint paths

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,26 +1,13 @@
 import express from 'express';
 const router = express.Router();
 
+import authController from '../controllers/authController.js';
 import employeeController from '../controllers/employeeController.js';
-import { hashPassword, comparePassword } from '../middlewares/bcryptMiddleware.js';
+import { comparePassword } from '../middlewares/bcryptMiddleware.js';
 
 router.post('/login', employeeController.getAuthEmployee, comparePassword, employeeController.loginEmployee);
 router.post('/logout', employeeController.logoutEmployee);
-router.post('/register', hashPassword, employeeController.createEmployee);
 
-const isAuthenticated = (req, res, next) => {
-  if (req.session && req.session.userId) {
-    return next();
-  }
-
-  res.status(401).json({ message: 'Unauthorized: Please log in.' });
-};
-
-router.get('/check-session', isAuthenticated, (req, res) => {
-  res.status(200).json({
-    isAuthenticated: true,
-    user: { id: req.session.userId },
-  });
-});
+router.get('/check-session', authController.isAuthenticated);
 
 export default router;
--- server/routes/sessionRoutes.js
+++ server/routes/sessionRoutes.js
@@ -4,6 +4,6 @@ const router = express.Router();
 
 import sessionController from '../controllers/sessionController.js';
 
-router.post('/set-date-range', sessionController.setDateRange);
+router.post('/sdr', sessionController.setDateRange);
 
 export default router;
