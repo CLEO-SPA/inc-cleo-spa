0a7eba556d8e29baa6aebd7a2af2ec307197134d
Arkar <arkarphyo@gmail.com>
2025-05-11 00:38:41 +0800

    feat: Refactor main.jsx to use React.StrictMode and remove theme.js file

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,11 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
-import myTheme from './theme.js';
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css"; // Tailwind base styles
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <ThemeProvider theme={myTheme}>
-      <CssBaseline />
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
       <App />
-    </ThemeProvider>
-  </StrictMode>
+  </React.StrictMode>
 );
