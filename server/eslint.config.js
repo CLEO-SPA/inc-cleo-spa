7600ef76a586b62a7bc5cdf6855479c176360dae
mintzic <minthitkhine2005@gmail.com>
2025-05-14 22:03:29 +0800

    chore: add ESLint and related packages to devDependencies

---
+++ server/eslint.config.js
@@ -0,0 +1,31 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    ...js.configs.recommended,
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      'no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
+      'no-console': 'off',
+      'no-undef': 'warn',
+      'no-unused-expressions': 'off',
+      'no-throw-literal': 'off',
+    },
+  },
+]);
