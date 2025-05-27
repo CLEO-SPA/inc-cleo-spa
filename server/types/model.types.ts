b9046e4d79bdc32a1b6b616890cf5878404bf10e
mintzic <minthitkhine2005@gmail.com>
2025-05-27 20:14:51 +0800

    feat: Define types for pagination and enhance employee and care package models

--- server/types/model.types.ts
+++ server/types/model.types.ts
@@ -1,14 +1,43 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export interface Employees {
   id?: string;
-  user_auth_id: string;
-  department_id: string;
-  position_id: string;
-  employee_code: string;
-  employee_contact: string;
-  employee_email: string;
-  employee_name: string;
+  user_auth_id?: string;
+  department_id?: string;
+  position_id?: string;
+  employee_code?: string;
+  employee_contact?: string;
+  employee_email?: string;
+  employee_name?: string;
+  created_at?: string;
+  updated_at?: string;
+  [key: string]: any;
+}
+
+export interface CarePackages {
+  id?: string;
+  care_package_name?: string;
+  care_package_remarks?: string;
+  care_package_price?: number;
+  care_package_customizable?: boolean;
+  status_id?: string;
+  created_by?: string;
+  last_updated_by?: string;
   created_at: string;
   updated_at: string;
+  [key: string]: any;
+}
+
+export interface MemberCarePackages {
+  id?: string;
+  member_id?: string;
+  employee_id?: string;
+  package_name?: string;
+  status_id?: string;
+  total_price?: number;
+  created_at?: string;
+  updated_at?: string;
+  package_remarks?: string;
+  [key: string]: any;
 }
 
 export interface SystemParameters {
