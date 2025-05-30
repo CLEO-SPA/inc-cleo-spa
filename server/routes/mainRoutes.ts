3db98203778064ba9f8e06b7d8d249588900ed55
mintzic <minthitkhine2005@gmail.com>
2025-05-30 16:04:07 +0800

    refactor: update service fields in schema and remove unused session table

--- server/routes/mainRoutes.ts
+++ server/routes/mainRoutes.ts
@@ -5,12 +5,14 @@ import simulationMiddleware from '../middlewares/simulationMiddleware.js';
 
 import authRoutes from './authRoutes.js';
 import sessionRoutes from './sessionRoutes.js';
+import carePackageRoutes from './cpRoutes.js';
 // import employeeRoutes from './employeeRoutes.js';
 
 router.use(simulationMiddleware);
 
 router.use('/auth', authRoutes);
 router.use('/session', sessionRoutes);
+router.use('/cp', carePackageRoutes);
 // router.use('/employee', employeeRoutes);
 
 // router.use('/sa', superAdminRoutes);
