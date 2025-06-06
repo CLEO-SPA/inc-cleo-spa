21252e045482f9d46738922516349f5ba4da6775
NPK <npkhaing2654@gmail.com>
2025-06-07 00:20:48 +0800

    feat(ReorderServicePage): add Reorder Service page with some drag-drop functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ import NotFoundPage from '@/pages/404Page';
 import ManageServicePage from '@/pages/ManageServicePage'
 import CreateServicePage from '@/pages/CreateServicePage';
 import UpdateServicePage from '@/pages/UpdateServicePage';
+import ReorderServicePage from '@/pages/ReorderServicePage';
 
 function App() {
   return (
@@ -29,6 +30,7 @@ function App() {
             <Route path='/manage-service' element={<ManageServicePage />} />
             <Route path='/create-service' element={<CreateServicePage />} />
             <Route path='/update-service/:service_id' element={<UpdateServicePage />} />
+            <Route path='/reorder-service' element={<ReorderServicePage />} />
 
             </Route>
             <Route path='/login' element={<LoginPage />} />
