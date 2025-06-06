dc6f78cf7c0c51069945313c307b3f683c184939
mintzic <minthitkhine2005@gmail.com>
2025-06-06 14:49:05 +0800

    Build: Uncomment employee, member, and appointment routes in mainRoutes; enable their usage in the application

--- server/routes/mainRoutes.ts
+++ server/routes/mainRoutes.ts
@@ -6,13 +6,17 @@ import simulationMiddleware from '../middlewares/simulationMiddleware.js';
 // import superAdminRoutes from './superAdminRoutes.js';
 import authRoutes from './authRoutes.js';
 import sessionRoutes from './sessionRoutes.js';
-// import employeeRoutes from './employeeRoutes.js';
+import employeeRoutes from './employeeRoutes.js';
+import memberRoutes from './memberRoutes.js';
+import appointmentRoutes from './appointmentRoutes.js';
 
 router.use(simulationMiddleware);
 
 router.use('/auth', authRoutes);
 router.use('/session', sessionRoutes);
-// router.use('/employee', employeeRoutes);
+router.use('/employee', employeeRoutes);
+router.use('/member', memberRoutes);
+router.use('/ab', appointmentRoutes);
 
 // router.use('/sa', superAdminRoutes);
 
