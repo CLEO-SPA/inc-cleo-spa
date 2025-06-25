7f85b40ab9c4a67c5112ed13371160dbb1945db7
yuetong2 <yuetong.inc@gmail.com>
2025-06-26 00:59:22 +0800

    feat: added manage mv to the sidebar menu

--- client/src/components/app-sidebar.jsx
+++ client/src/components/app-sidebar.jsx
@@ -84,6 +84,11 @@ const data = {
           title: 'Manage Voucher Templates',
           url: '/voucher-template',
         },
+        {
+          title: 'Manage Member Vouchers',
+          url: '/mv'
+
+        }
       ],
     },
     {
