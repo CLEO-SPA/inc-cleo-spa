130ac3f99705d4ede1fe7f446b21451f8263c03a
mintzic <minthitkhine2005@gmail.com>
2025-06-29 11:32:29 +0800

    refactor(nav-user): clean up commented code and improve readability refactor(voucherRoute): standardize import quotes and clean up commented log

--- server/routes/voucherRoute.ts
+++ server/routes/voucherRoute.ts
@@ -1,14 +1,14 @@
 import express from 'express';
 const router = express.Router();
 
-import voucherController from "../controllers/voucherController.js";
+import voucherController from '../controllers/voucherController.js';
 
-console.log("✅ Voucher routes file is loaded");
+// console.log("✅ Voucher routes file is loaded");
 
-router.get("/", voucherController.getVoucherTemplatesDetailsHandler);
-router.get("/m", voucherController.getMemberVoucherDetailsHandler);
-router.get("/vm", voucherController.getVoucherTemplateNamesHandler);
-router.post("/transfer", voucherController.transferVoucherDetailsHandler);
+router.get('/', voucherController.getVoucherTemplatesDetailsHandler);
+router.get('/m', voucherController.getMemberVoucherDetailsHandler);
+router.get('/vm', voucherController.getVoucherTemplateNamesHandler);
+router.post('/transfer', voucherController.transferVoucherDetailsHandler);
 
 // Testing purposes
 // http://localhost:3000/api/v/cFoc?memberId=51&voucher_id=32
